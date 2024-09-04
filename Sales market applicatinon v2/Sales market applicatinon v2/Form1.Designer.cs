4f53522bc44ccb7b96e559ce29b94a8dffd9ef24
Fouad <jazaerefouad@gmail.com>
2024-09-04 13:36:25 +0300

    this is a new design for project

---
+++ Sales market applicatinon v2/Sales market applicatinon v2/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Sales_market_applicatinon_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox_logo = new System.Windows.Forms.PictureBox();
+            this.lb_login = new System.Windows.Forms.Label();
+            this.textBox_user = new System.Windows.Forms.TextBox();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox_show = new System.Windows.Forms.CheckBox();
+            this.lbshow = new System.Windows.Forms.Label();
+            this.btncheck = new System.Windows.Forms.Button();
+            this.btn_sign = new System.Windows.Forms.Button();
+            this.pictureBox_exit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_exit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_logo
+            // 
+            this.pictureBox_logo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_logo.Image")));
+            this.pictureBox_logo.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox_logo.Name = "pictureBox_logo";
+            this.pictureBox_logo.Size = new System.Drawing.Size(344, 503);
+            this.pictureBox_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_logo.TabIndex = 0;
+            this.pictureBox_logo.TabStop = false;
+            // 
+            // lb_login
+            // 
+            this.lb_login.AutoSize = true;
+            this.lb_login.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_login.ForeColor = System.Drawing.Color.White;
+            this.lb_login.Location = new System.Drawing.Point(550, -1);
+            this.lb_login.Name = "lb_login";
+            this.lb_login.Size = new System.Drawing.Size(131, 45);
+            this.lb_login.TabIndex = 4;
+            this.lb_login.Text = "Log in ";
+            // 
+            // textBox_user
+            // 
+            this.textBox_user.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_user.Location = new System.Drawing.Point(495, 161);
+            this.textBox_user.MaxLength = 26;
+            this.textBox_user.Name = "textBox_user";
+            this.textBox_user.Size = new System.Drawing.Size(245, 26);
+            this.textBox_user.TabIndex = 5;
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_pass.Location = new System.Drawing.Point(495, 308);
+            this.textBox_pass.MaxLength = 15;
+            this.textBox_pass.Multiline = true;
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.PasswordChar = '*';
+            this.textBox_pass.Size = new System.Drawing.Size(245, 25);
+            this.textBox_pass.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(544, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "User name ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(553, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
+            // checkBox_show
+            // 
+            this.checkBox_show.AutoSize = true;
+            this.checkBox_show.Location = new System.Drawing.Point(713, 315);
+            this.checkBox_show.Name = "checkBox_show";
+            this.checkBox_show.Size = new System.Drawing.Size(15, 14);
+            this.checkBox_show.TabIndex = 10;
+            this.checkBox_show.UseVisualStyleBackColor = true;
+            this.checkBox_show.CheckedChanged += new System.EventHandler(this.checkBox_show_CheckedChanged);
+            // 
+            // lbshow
+            // 
+            this.lbshow.AutoSize = true;
+            this.lbshow.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbshow.Location = new System.Drawing.Point(756, 316);
+            this.lbshow.Name = "lbshow";
+            this.lbshow.Size = new System.Drawing.Size(40, 16);
+            this.lbshow.TabIndex = 11;
+            this.lbshow.Text = "Show";
+            // 
+            // btncheck
+            // 
+            this.btncheck.BackColor = System.Drawing.Color.SkyBlue;
+            this.btncheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncheck.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncheck.Location = new System.Drawing.Point(773, 445);
+            this.btncheck.Name = "btncheck";
+            this.btncheck.Size = new System.Drawing.Size(97, 44);
+            this.btncheck.TabIndex = 12;
+            this.btncheck.Text = "Check";
+            this.btncheck.UseVisualStyleBackColor = false;
+            this.btncheck.Click += new System.EventHandler(this.btncheck_Click);
+            // 
+            // btn_sign
+            // 
+            this.btn_sign.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_sign.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sign.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sign.ForeColor = System.Drawing.Color.White;
+            this.btn_sign.Location = new System.Drawing.Point(350, 445);
+            this.btn_sign.Name = "btn_sign";
+            this.btn_sign.Size = new System.Drawing.Size(97, 44);
+            this.btn_sign.TabIndex = 14;
+            this.btn_sign.Text = "Sign up";
+            this.btn_sign.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox_exit
+            // 
+            this.pictureBox_exit.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_exit.Image")));
+            this.pictureBox_exit.Location = new System.Drawing.Point(836, -1);
+            this.pictureBox_exit.Name = "pictureBox_exit";
+            this.pictureBox_exit.Size = new System.Drawing.Size(44, 48);
+            this.pictureBox_exit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_exit.TabIndex = 15;
+            this.pictureBox_exit.TabStop = false;
+            this.pictureBox_exit.Click += new System.EventHandler(this.pictureBox_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(882, 501);
+            this.Controls.Add(this.pictureBox_exit);
+            this.Controls.Add(this.btn_sign);
+            this.Controls.Add(this.btncheck);
+            this.Controls.Add(this.lbshow);
+            this.Controls.Add(this.checkBox_show);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_pass);
+            this.Controls.Add(this.textBox_user);
+            this.Controls.Add(this.lb_login);
+            this.Controls.Add(this.pictureBox_logo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sales market v2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_exit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_logo;
+        private System.Windows.Forms.Label lb_login;
+        private System.Windows.Forms.TextBox textBox_user;
+        private System.Windows.Forms.TextBox textBox_pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox_show;
+        private System.Windows.Forms.Label lbshow;
+        private System.Windows.Forms.Button btncheck;
+        private System.Windows.Forms.Button btn_sign;
+        private System.Windows.Forms.PictureBox pictureBox_exit;
+    }
+}
+
---
+++ Sales market applicatinon v2/Sales market applicatinon v2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales_market_applicatinon_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_minmize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btncheck_Click(object sender, EventArgs e)
+        {
+            if (textBox_pass.Text == "" && textBox_user.Text == "")
+            {
+                MessageBox.Show("please enter the user name and password", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (textBox_pass.Text != "" && textBox_user.Text == "")
+            {
+                MessageBox.Show("the user name field is empty", "Warning ?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (textBox_user.Text != "" && textBox_pass.Text == "")
+            {
+                MessageBox.Show("the password field is empty ", "Warning ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (textBox_pass.Text.Length <= 5)
+            {
+                MessageBox.Show("Please enter a number of characters between 5 and 15 in the  password", "Error",
+                MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBox_show_CheckedChanged(object sender, EventArgs e)
+        {
+           textBox_pass.UseSystemPasswordChar= checkBox_show.Checked?
+           textBox_pass.UseSystemPasswordChar = true : textBox_pass.UseSystemPasswordChar = false;   
+        }
+
+        
+
+        private void pictureBox_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+    }
+}
+
+   
+
+
