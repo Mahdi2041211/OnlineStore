4a7e8d936691aaffad93e5691c00eab68a698487
Ayhamkassar <168710057+Ayhamkassar@users.noreply.github.com>
2024-09-07 22:00:01 +0300

    here take it

---
+++ show screen/show screen/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace show_screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clothes = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.laptops = new System.Windows.Forms.PictureBox();
+            this.books = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clothes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.laptops)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.books)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(0, 0);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(733, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Logo";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Location = new System.Drawing.Point(13, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 187);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.BlueViolet;
+            this.pictureBox6.Location = new System.Drawing.Point(833, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(282, 187);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox7.Location = new System.Drawing.Point(555, 0);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(282, 187);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 4;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.pictureBox5.Location = new System.Drawing.Point(277, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(282, 187);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(282, 187);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox4.Location = new System.Drawing.Point(-730, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(730, 184);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lavender;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(290, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 24);
+            this.textBox1.TabIndex = 5;
+            // 
+            // clothes
+            // 
+            this.clothes.BackColor = System.Drawing.Color.Transparent;
+            this.clothes.BackgroundImage = global::show_screen.Properties.Resources.البسة1;
+            this.clothes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.clothes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clothes.Location = new System.Drawing.Point(13, 447);
+            this.clothes.Name = "clothes";
+            this.clothes.Size = new System.Drawing.Size(159, 156);
+            this.clothes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.clothes.TabIndex = 11;
+            this.clothes.TabStop = false;
+            this.clothes.MouseLeave += new System.EventHandler(this.clothes_MouseLeave);
+            this.clothes.MouseMove += new System.Windows.Forms.MouseEventHandler(this.clothes_MouseMove);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox11.Location = new System.Drawing.Point(178, 284);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(429, 319);
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox9.Location = new System.Drawing.Point(613, 447);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(159, 156);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            // 
+            // laptops
+            // 
+            this.laptops.BackgroundImage = global::show_screen.Properties.Resources.لابتوبات;
+            this.laptops.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.laptops.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.laptops.Location = new System.Drawing.Point(613, 285);
+            this.laptops.Name = "laptops";
+            this.laptops.Size = new System.Drawing.Size(159, 156);
+            this.laptops.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.laptops.TabIndex = 8;
+            this.laptops.TabStop = false;
+            this.laptops.MouseLeave += new System.EventHandler(this.laptops_MouseLeave);
+            this.laptops.MouseMove += new System.Windows.Forms.MouseEventHandler(this.laptops_MouseMove);
+            // 
+            // books
+            // 
+            this.books.BackColor = System.Drawing.Color.Transparent;
+            this.books.BackgroundImage = global::show_screen.Properties.Resources.قرطاسية;
+            this.books.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.books.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.books.Location = new System.Drawing.Point(13, 285);
+            this.books.Name = "books";
+            this.books.Size = new System.Drawing.Size(159, 156);
+            this.books.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.books.TabIndex = 6;
+            this.books.TabStop = false;
+            this.books.MouseLeave += new System.EventHandler(this.books_MouseLeave);
+            this.books.MouseMove += new System.Windows.Forms.MouseEventHandler(this.books_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::show_screen.Properties.Resources.lens;
+            this.pictureBox2.Location = new System.Drawing.Point(520, 13);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.clothes);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.laptops);
+            this.Controls.Add(this.books);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clothes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.laptops)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.books)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox books;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox laptops;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox clothes;
+    }
+}
+
---
+++ show screen/show screen/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace show_screen
+{
+    public partial class Form1 : Form
+    {
+        private int x = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.FillRectangle(Brushes.Blue,x,0,2700,50);
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point Current = panel1.AutoScrollPosition;
+            Current.X = -Current.X;
+            if (Current.X <= panel1.HorizontalScroll.Maximum)
+            {
+                Current.X += 20;
+                panel1.AutoScrollPosition = Current;
+                if (Current.X == 329)
+                {
+                    Current.X = panel1.HorizontalScroll.Maximum;
+                }
+            }
+            if (Current.X == panel1.HorizontalScroll.Maximum)
+            {
+                Current.X = 0;
+                panel1.AutoScrollPosition = Current;
+                timer1.Stop();
+            }
+        }
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+        }
+
+        private void books_MouseMove(object sender, MouseEventArgs e)
+        {
+            books.Size = new Size(200, 200);
+            books.BringToFront();
+        }
+
+        private void books_MouseLeave(object sender, EventArgs e)
+        {
+            books.Size = new Size(140, 120);
+        }
+
+        private void clothes_MouseMove(object sender, MouseEventArgs e)
+        {
+            clothes.Size = new Size(200, 200);
+            clothes.BringToFront();
+        }
+
+        private void clothes_MouseLeave(object sender, EventArgs e)
+        {
+            clothes.Size = new Size(140, 120);
+        }
+
+        private void laptops_MouseLeave(object sender, EventArgs e)
+        {
+            laptops.Size = new Size(140, 120);
+        }
+
+        private void laptops_MouseMove(object sender, MouseEventArgs e)
+        {
+            laptops.Size = new Size(200, 200);
+            laptops.BringToFront();
+        }
+    }
+}
